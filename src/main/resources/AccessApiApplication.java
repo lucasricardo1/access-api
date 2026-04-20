5218136f8562d85b08e21181c12888bab27cb1cc
Lucas Ricardo <lucasricardox.o@gmail.com>
2026-04-19 21:36:57 -0300

    First commit, starting repo

---
+++ src/main/resources/AccessApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasricardo1.access_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lucasricardo1/access_api/AccessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasricardo1.access_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
